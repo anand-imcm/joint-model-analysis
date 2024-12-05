da48171ab22b107512bb9db77b385f0b2b33f767
anand-imcm <anand.maurya@well.ox.ac.uk>
2024-12-05 14:37:52 +0000

    removed trycatch

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -4,9 +4,7 @@ suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(survival))
 suppressPackageStartupMessages(library(JMbayes2))
 suppressPackageStartupMessages(library(nlme))
-
 set.seed(2222)
-
 ########################################################################### PREP
 print(paste0("START loading inputs ", format(Sys.time(), "%H:%M:%S"), " on ", Sys.Date()))
 args <- commandArgs(trailingOnly = TRUE)
@@ -14,7 +12,6 @@ serum <- read.csv(args[1])
 demog <- read.csv(args[2])
 protein <- args[3]
 print(paste0("END loading inputs ", format(Sys.time(), "%H:%M:%S"), " on ", Sys.Date()))
-
 print(paste0("START merging data ", format(Sys.time(), "%H:%M:%S"), " on ", Sys.Date()))
 long <- merge(serum,demog,by.x="SampleID",by.y="SERUM_OLINK_MANIFEST",all.x=T) %>%
   filter(GROUP=="AMYOTROPHIC LATERAL SCLEROSIS") %>%
@@ -32,19 +29,18 @@ surv <- long %>%
   filter(row_number() == 1)
 print(paste0("END filter ", format(Sys.time(), "%H:%M:%S"), " on ", Sys.Date()))
 ################################################################ EXAMPLE PROTEIN
-
 print(paste0("START survival::coxph ", format(Sys.time(), "%H:%M:%S"), " on ", Sys.Date()))
 survFit <- survival::coxph(Surv(Time, death) ~ 1, data = surv, x = TRUE)
 print(paste0("END survival::coxph ", format(Sys.time(), "%H:%M:%S"), " on ", Sys.Date()))
 print(paste0("START nlme::lme ", format(Sys.time(), "%H:%M:%S"), " on ", Sys.Date()))
 lme_protein <- nlme::lme(as.formula(paste(protein, "~ DELTA")), 
-                          random = ~ DELTA | IMCM_ID, 
-                          data = long,
-                          control = lmeControl(opt = 'optim'))
+                         random = ~ DELTA | IMCM_ID, 
+                         data = long,
+                         control = lmeControl(opt = 'optim'))
 print(paste0("END nlme::lme ", format(Sys.time(), "%H:%M:%S"), " on ", Sys.Date()))
 print(paste0("START JMbayes2::jm ", format(Sys.time(), "%H:%M:%S"), " on ", Sys.Date()))
 joint_protein <- JMbayes2::jm(survFit, lme_protein, time_var = "DELTA",
-                              id_var="IMCM_ID", n_iter = 10000L, n_burnin = 1000L)
+                           id_var="IMCM_ID", n_iter = 10000L, n_burnin = 1000L)
 print(paste0("END JMbayes2::jm ", format(Sys.time(), "%H:%M:%S"), " on ", Sys.Date()))
 print(paste0("START joint_protein_results ", format(Sys.time(), "%H:%M:%S"), " on ", Sys.Date()))
 joint_protein_results <- data.frame(row.names = NULL,
@@ -63,5 +59,4 @@ print(paste0("END write.csv ", format(Sys.time(), "%H:%M:%S"), " on ", Sys.Date(
 print(paste0("START saveRDS ", format(Sys.time(), "%H:%M:%S"), " on ", Sys.Date()))
 saveRDS(joint_protein,paste0(protein,"_joint_results.RDS"),compress = "gzip")
 print(paste0("END saveRDS ", format(Sys.time(), "%H:%M:%S"), " on ", Sys.Date()))
-
 print(paste0("END script ", format(Sys.time(), "%H:%M:%S"), " on ", Sys.Date()))
